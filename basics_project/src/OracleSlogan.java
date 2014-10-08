d8faf4baa04f3996a667bda467973198fbc3137e
Sashka <Aliaksandra_Chernyavskaya@epam.com>
2014-10-08 10:08:43 +0300

    Converted to Maven project

--- basics_project/src/OracleSlogan.java
+++ basics_project/src/OracleSlogan.java
@@ -1,8 +1,16 @@
+import org.apache.commons.lang.StringUtils;
+
 
 public class OracleSlogan {
 
 	public static void main(String[] args) {
 		System.out.println("Enabling the Information Age");
+		
+		String test = "test";
+		
+		if (StringUtils.isEmpty(test)) {
+			System.out.println("is empty!");
+		}
 	}
 
 }
